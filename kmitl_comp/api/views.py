d80719607211da80d607aa6a470bca70933938cb
ryuinw123 <47142982+ryuinw123@users.noreply.github.com>
2022-12-07 17:53:18 +0700

    New BE

--- kmitl_comp/api/views.py
+++ kmitl_comp/api/views.py
@@ -3,7 +3,13 @@ from django.core.exceptions import BadRequest
 from django.forms import ValidationError
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.http import JsonResponse
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+import os
+
 from .models import *
 
 ########
@@ -49,12 +55,20 @@ def getLocationQuery(request) -> None:
         longitude = data["longitude"]
         x = requests.get(f"https://api.mapbox.com/geocoding/v5/mapbox.places/{longitude},{latitude}.json?access_token=pk.eyJ1Ijoicnl1aW53MTIzIiwiYSI6ImNsODV5M21odjB0dXAzbm9lZDhnNXVoY2UifQ.IiTAr5ITOUcCVjPjWiRe1w&limit=1")
         data = x.json()
-        feature = data["features"][0]
 
-        response = {
-            "place" : feature["text"],
-            "address" : feature["place_name"]
-        }
+        if (data):
+            feature = data["features"][0]
+
+            response = {
+                "place" : feature["text"],
+                "address" : feature["place_name"]
+            }
+        else:
+            response = {
+                "place" : "",
+                "address" : ""
+            }
+        
 
         return JsonResponse(response,safe=False)
     
@@ -229,16 +243,40 @@ def postUserData(request) -> None:
 
 #/********************************* test method ********************************/
 
-testlst = [{"name":"John", "id":30, "latitude":"13.779677724153272" , "longitude" : "100.67650630259816","description" : "noob"},{"name":"Cena", "id":31, "latitude":"13.779677724153272" , "longitude" : "100.97650630259816","description" : "noob2"}]
+testlst = [{
+    "id":30,
+    "latitude":"13.779677724153272" ,
+    "longitude" : "100.67650630259816",
+    "description" : "noob",
+    "address" : "9/35 how",
+    "place":"Tun School",
+    "type" : "School",
+    "imageLink": ["https://m.media-amazon.com/images/G/01/gc/designs/livepreview/a_generic_white_10_us_noto_email_v2016_us-main._CB627448186_.png","https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fd1e00ek4ebabms.cloudfront.net%2Fproduction%2Fbbdd464a-04e2-4a5a-8cbe-198465d68fdb.jpg?fit=scale-down&source=next&width=700"]},
+    {"place":"Cena",
+    "id":31,
+    "latitude":"13.779677724153272",
+    "longitude" : "100.97650630259816",
+    "description" : "noob2",
+    "address" : "How the heck",
+    "place" : "TunnelSchool",
+    "type" : "Pokemon",
+    "imageLink":["https://pbs.twimg.com/profile_images/1250516096907571200/adjxWadZ_400x400.jpg"]
+    }]
 def helloWorld(request):
     print(testlst)
 
     return JsonResponse(testlst,safe = False)
-    
-@csrf_exempt
+
+@api_view(['POST'])
 def testpost(request):
+    print(request.data)
     if request.method == 'POST':
         data_dict = request.POST
+
+        file = request.data['image'] # or self.files['image'] in your form
+        path = default_storage.save(f'tmp/image.png', ContentFile(file.read()))
+        tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+        
         print(data_dict)
 
         letters = string.ascii_lowercase
@@ -247,7 +285,7 @@ def testpost(request):
         longitude = data_dict['longitude']
         testlst.append({"name":randomword, "id":random.randint(1000,2000), "latitude":latitude , "longitude" : longitude,"description" : "noob2"})
         print(testlst)
-        return HttpResponse('')
+        return HttpResponse()
 
 @csrf_exempt
 def checkToken(request):
